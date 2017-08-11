c240fe20d8de1f685b77e05c85335ff045e4dbad
Matthieu Wipliez <matthieu@codingame.com>
2017-08-11 15:57:51 +0200

    call checkPropTypes explicitly

--- counter/src/counter-with-default-props.js
+++ counter/src/counter-with-default-props.js
@@ -5,15 +5,25 @@ import PropTypes from 'prop-types'
 // }
 
 // destructure props directly into variables
-const Counter = ({initialCount, labels}) =>
-  <div>
-    <p>Number of items: {initialCount}</p>
-    {labels.map(label => <button key={label}>{label}</button>)}
-  </div>
+const Counter = (props) => {
+  // { autofold
+  // check PropTypes explicitly because we are using the production version of React
+  // normally in development this is done for you by React
+  PropTypes.checkPropTypes(Counter.propTypes, props, 'prop', 'Counter');
+  // }
+
+  let {initialCount, labels} = props;
+  return (
+    <div>
+      <p>Number of items: {initialCount}</p>
+      {labels.map(label => <button key={label}>{label}</button>)}
+    </div>
+  )
+}
 
 Counter.propTypes = {
   initialCount: PropTypes.number.isRequired,
-  labels: PropTypes.objectOf(PropTypes.string)
+  labels: PropTypes.arrayOf(PropTypes.string)
 }
 
 // FIXME: add a default value for the initialCount property
--- counter/src/counter-with-prop-types.js
+++ counter/src/counter-with-prop-types.js
@@ -2,11 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
-const Counter = props =>
-  <div>
-    <p>Number of items: {props.initialCount}</p>
-    {['+', '-'].map(label => <button key={label}>{label}</button>)}
-  </div>
+const Counter = props => {
+  // { autofold
+  // check PropTypes explicitly because we are using the production version of React
+  // normally in development this is done for you by React
+  PropTypes.checkPropTypes(Counter.propTypes, props, 'prop', 'Counter');
+  // }
+
+  return (
+    <div>
+      <p>Number of items: {props.initialCount}</p>
+      {['+', '-'].map(label => <button key={label}>{label}</button>)}
+    </div>
+  )
+}
 
 Counter.propTypes = {
   initialCount: PropTypes.number.isRequired
